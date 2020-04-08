a4193d22dc39cd2196b7daf5e83b989ec83f1e49
Pampa Nelson <pampa.nelson@gmail.com>
2020-04-08 21:10:10 +0800

    add hashids lib, fix path for ssl
    vscod autoload not work yet

--- __init__.py
+++ __init__.py
@@ -10,26 +10,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-import sys
-import pathlib
+
 import os
+import pathlib
+import sys
+# from . import auto_load
 
-bl_info = {
-    "name" : "brender_test",
-    "author" : "Pampa Nie",
-    "description" : "",
-    "blender" : (2, 82, 0),
-    "version" : (0, 0, 1),
-    "location" : "",
-    "warning" : "",
-    "category" : "Generic"
-}
-
-
-from . import auto_load
-auto_load.init()
-
-print('hello')
 brender_addon_path = pathlib.Path(__file__).parent.absolute()
 sep = os.path.sep
 lib_path = str(brender_addon_path) + sep + 'pika-1.1.0'
@@ -41,17 +27,46 @@ sys.path.append(lib_path)
 lib_path = str(brender_addon_path) + sep + 'qiniu-python-sdk-7.2.8'
 sys.path.append(lib_path)
 
+lib_path = str(brender_addon_path) + sep + 'hashids-1.2.0'
+sys.path.append(lib_path)
 
 import pika
 import avro
 import ssl
 import qiniu
-print('world')
-print (qiniu)
-print(dir(qiniu))
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
+
+bl_info = {
+    "name": "brender_test",
+    "author": "Pampa Nie",
+    "description": "",
+    "blender": (2, 82, 0),
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "category": "Generic"
+}
+
+
+# auto_load.init()
+
+print('hello')
+
+import binascii
+from hashids import Hashids
+hashids = Hashids()
+
+hex1 = binascii.hexlify('FmER_mJpSutxsBTMnFacisNjy_Ib'.encode())
+print(hex1)
+hash1 = hashids.encode_hex(hex1.decode())
+print(hash1)
+
+hex2 = hashids.decode_hex(hash1)
+print(hex2)
+s = binascii.unhexlify(hex2)
+print(s.decode())
 
 # for method_frame, properties, body in channel.consume('test'):
 #     # Display the message parts and acknowledge the message
@@ -67,6 +82,7 @@ from avro.io import DatumReader, DatumWriter
 # print('Requeued %i messages' % requeued_messages)
 # connection.close()
 
+
 def test_pika():
     print('test pika ssl')
 
@@ -80,34 +96,42 @@ def test_pika():
     # print(" [x] Sent 'Hello World!' from py")
     # connection.close()
 
+    brender_addon_path = pathlib.Path(__file__).parent.absolute()
+    sep = os.path.sep
+    sslPath = str(brender_addon_path) + sep + 'ssl' + sep
+    context = ssl.create_default_context(cafile= sslPath + "cacert.pem")
+    context.set_ciphers('ALL:@SECLEVEL=0')
 
-    context = ssl.create_default_context(cafile="./ssl/cacert.pem")
-    context.set_ciphers('ALL:@SECLEVEL=0') # 
-
-    context.load_cert_chain(certfile="./ssl/brender-client.cert.pem",
-                            keyfile="./ssl/brender-client.key.pem",
+    context.load_cert_chain(certfile=sslPath + "brender-client.cert.pem",
+                            keyfile=sslPath + "brender-client.key.pem",
                             password="dMokP0brnSeGsphGCfsH41Yr2cwDLauB")
 
     credentials = pika.PlainCredentials('guest', 'guest')
     ssl_options = pika.SSLOptions(context, 'amqps.brender.cn')
     conn_params = pika.ConnectionParameters(
-                                            host='amqps.brender.cn',
-                                            port=5671,
-                                            ssl_options=ssl_options,
-                                            credentials=credentials,
-                                            heartbeat=30)
+        host='amqps.brender.cn',
+        port=5671,
+        ssl_options=ssl_options,
+        credentials=credentials,
+        heartbeat=30)
 
     with pika.BlockingConnection(conn_params) as conn:
         ch = conn.channel()
         ch.queue_declare("foobar")
-        ch.basic_publish("", "foobar", "Hello, world! with ssl from blender by pika")
+        ch.basic_publish(
+            "", "foobar", "Hello, world! with ssl from blender by pika")
         # print(ch.basic_get("foobar"))
 
 
 test_pika()
 
+
 def register():
-    auto_load.register()
+    # auto_load.register()
+    pass
+
 
 def unregister():
-    auto_load.unregister()
+    # auto_load.unregister()
+    pass
+
--- back/README.md
+++
@@ -1,1 +0,0 @@
-# brender_addon
